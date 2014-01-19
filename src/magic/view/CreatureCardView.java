67f0b87fe464a1b3aa8e79d1e78add4f90af8a05
jbauschatz <john.bauschatz@my.wheaton.edu>
2014-01-19 17:23:57 -0600

    Visualizing damage dealt to Creatures

--- src/magic/view/CreatureCardView.java
+++ src/magic/view/CreatureCardView.java
@@ -7,9 +7,16 @@ import java.awt.*;
 public class CreatureCardView extends CardView {
 
     protected void drawContent(Graphics g) {
-        String powerToughness = creature.getCurrentPower() + "/" + creature.getCurrentToughness();
         g.setFont(font);
-        g.drawString(powerToughness, inset + 2, getHeight() - inset - 2);
+        String toughness = creature.getCurrentToughness() + "";
+        int toughnessWidth = fontMetrics.stringWidth(toughness);
+        g.setColor(creature.getCurrentToughness() < creature.printedToughness ? Color.RED : Color.BLACK);
+        g.drawString(toughness, getWidth()-inset-2-toughnessWidth, getHeight()-inset-2);
+
+        String power = creature.getCurrentPower() + "/";
+        int powerWidth = fontMetrics.stringWidth(power);
+        g.setColor(Color.black);
+        g.drawString(power, getWidth()-inset-2-toughnessWidth-powerWidth, getHeight()-inset-2);
     }
 
     public CreatureCardView(Creature creature) {
